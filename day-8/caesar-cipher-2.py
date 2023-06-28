d4ced2dc31bb2ddada7cbc7aa77e3367b685b196
Madhusudhanan Ramachandran <madhusudhanan.ramachandran@cdk.com>
2023-06-28 13:35:20 -0400

    Further reduced the size of the code.

--- day-8/caesar-cipher-2.py
+++ day-8/caesar-cipher-2.py
@@ -9,18 +9,16 @@ no_of_alphabets = len(alphabet)
 
 def caesar(text, shift, direction):
     output_string = ""
+    if direction == "decode":
+        shift *= -1
+
     for ltr in text:
         if ltr in alphabet:
-            if direction == "encode":
-                new_position = alphabet.index(ltr) + shift
-                if new_position > last_index:
-                    new_position = new_position - no_of_alphabets
-                # print(f"{ltr} => {alphabet[new_position]}")
-                output_string += alphabet[new_position]
-            if direction == "decode":
-                new_position = alphabet.index(ltr) - shift
-                # print(f"{ltr} => {alphabet[new_position]}")
-                output_string += alphabet[new_position]
+            new_position = alphabet.index(ltr) + shift
+            if new_position > last_index:
+                new_position = new_position - no_of_alphabets
+            # print(f"{ltr} => {alphabet[new_position]}")
+            output_string += alphabet[new_position]
         else:
             output_string += ltr
     print(f"The {direction}d text is {output_string}")
