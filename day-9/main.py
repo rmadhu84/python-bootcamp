d6d871d5f5973f987e0bcfd643f1f642d0ec7abd
Madhusudhanan Ramachandran <madhusudhanan.ramachandran@cdk.com>
2023-07-03 11:00:31 -0400

    Day-9. Add a new item to dictionary

--- day-9/main.py
+++ day-9/main.py
@@ -1,3 +1,7 @@
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
                           "Function": "A piece of code that you can easily call over and over again."}
-print(programming_dictionary["Bug"])
+print(programming_dictionary)
+
+# Adding a new item
+programming_dictionary["Loop"] = "The action of doing something over a fixed number of times."
+print(programming_dictionary)
